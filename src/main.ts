32f76d6a69f90690b371f5b5409f5551b315eee4
alissonbgs <alissonbgs@outlook.com>
2023-07-31 20:21:52 -0300

    input validations and error handling

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   
   const config = new DocumentBuilder()
-    .setTitle('The NestJS + Prisma Template')
-    .setDescription('This is NestJS + Prisma Template documentation')
+    .setTitle('Median')
+    .setDescription('The Median API description')
     .setVersion('0.1')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('doc', app, document);
+  SwaggerModule.setup('api', app, document);
+
 
   await app.listen(3000);
 }
